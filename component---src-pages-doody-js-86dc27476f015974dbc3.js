c70af3434b5cf9c2beb71f916ae4a5a4f17eee8f
rickD <debboutr@gmail.com>
2019-01-19 17:21:57 -0800

    Updates

---
+++ component---src-pages-doody-js-86dc27476f015974dbc3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{138:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),r=n(142);t.default=function(){return l.a.createElement("div",{style:{color:"teal"}},l.a.createElement(r.a,null),l.a.createElement("h1",null,"About Gatsby"),l.a.createElement("p",null,"Such wow. Very React."))}},142:function(e,t,n){"use strict";var a=n(0),l=n.n(a);t.a=function(e){return l.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-doody-js-86dc27476f015974dbc3.js.map
